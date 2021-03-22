41dd914a397c3cf9b194850625f3a5f0d4467a57
Shashank HP <57084282+shashankhp@users.noreply.github.com>
2021-03-22 20:36:54 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+	if request.method == "POST":
+		link = request.form["link"]
+		yt = YouTube(link)
+		video = yt.streams.filter(file_extension='mp4').order_by('resolution').desc()
+		video.first().download()
+		return redirect(url_for("login"))
+	else :
 		return render_template("index.html")
 
-@app.route("/<lk>")
-def login(lk):
-    return f"<h1>{lk}</h1>"
+@app.route("/downloaded")
+def dwldpage():
+    return render_template("dwpage.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
