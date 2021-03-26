84bbc0997e8835a769123a6bb8e24365f462792c
Shashank HP <57084282+shashankhp@users.noreply.github.com>
2021-03-26 21:37:29 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,24 +2,17 @@ from flask import Flask, redirect, url_for, render_template, request, flash, ses
 from pytube import YouTube
 
 app = Flask(__name__)
-app.secret_key = "SSKhp"
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-	try:
-		if request.method == "POST":
-			link = request.form["link"]
-			session["link"] = link
-			yt = YouTube(link)
-			video = yt.streams.filter(file_extension='mp4').order_by('resolution').desc()
-			video.first().download()
-			flash("Video Downloaded Successfully")
-			return render_template("index.html")
-		else :
-			return render_template("index.html")
-	except(Exception):
-		flash("Video inaccessible. Check the Link and try again!")
+	if request.method == "POST":
+		link = request.form["link"]
+		yt = YouTube(link)
+		video = yt.streams.filter(file_extension='mp4').order_by('resolution').desc()
+		video.first().download()
 		return render_template("index.html")
-	
+	else :
+		return render_template("index.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
