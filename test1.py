da501fcac5a1a070c8b82dc7ee784034bbf4f858
Shashank HP <57084282+shashankhp@users.noreply.github.com>
2021-03-29 11:11:32 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, flash, session
+from flask import Flask, redirect, url_for, render_template, request, flash
 from pytube import YouTube
 
 app = Flask(__name__)
