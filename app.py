9deb358696372d0f09049212bb3806f72ed29403
Shashank HP <57084282+shashankhp@users.noreply.github.com>
2021-03-30 21:17:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,23 @@ def home():
 			session["link"] = link
 			yt = YouTube(link)
 			video = yt.streams.filter(file_extension='mp4').order_by('resolution').desc()
-			video.first().download()
+			video.first().download(filename='Video')
 			flash("Video Downloaded Successfully")
-			return render_template("index.html")
+			return redirect(url_for("download_file"))
 		else :
 			return render_template("index.html")
 	except(Exception):
 		flash("Video inaccessible. Check the Link and try again!")
 		return render_template("index.html")
+
+
+@app.route('/download')
+def download_file():
+    #path = "html2pdf.pdf"
+	#path = "info.xlsx"
+	path = "Video.mp4"
+	#path = "sample.txt"
+	return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
 	app.run(debug=True)
